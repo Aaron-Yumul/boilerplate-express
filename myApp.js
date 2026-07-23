76ae0a3c0f721432aea1e9504f594e865a00cb98
Aaron John Yumul <aaronjohnyumul526@gmail.com>
2026-07-23 13:51:51 +0800

    Update myApp.js
    Signed-off-by: Aaron John Yumul <aaronjohnyumul526@gmail.com>

--- myApp.js
+++ myApp.js
@@ -52,10 +52,17 @@ app.get('/:word/echo', function(req, res) {
   });
 });
 
-// Query parameter
-app.get('/name', function(req, res) {
+// GET /name
+app.route('/name').get(function(req, res) {
   res.json({
     name: req.query.first + ' ' + req.query.last
+  });
+})
+
+// POST /name
+.post(function(req, res) {
+  res.json({
+    name: req.body.first + ' ' + req.body.last
   });
 });
 
