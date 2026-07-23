62492a6800f5c84d7a13d7a939629f7ba3ef8aee
Aaron John Yumul <aaronjohnyumul526@gmail.com>
2026-07-23 13:47:37 +0800

    Update myApp.js
    Signed-off-by: Aaron John Yumul <aaronjohnyumul526@gmail.com>

--- myApp.js
+++ myApp.js
@@ -1,6 +1,34 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
 
+// Root-level logger middleware
+app.use(function(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
+// Serve static assets
+app.use('/public', express.static(__dirname + '/public'));
+
+// Home route
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// JSON route
+app.get('/json', function(req, res) {
+  let message = 'Hello json';
+
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    message = message.toUpperCase();
+  }
+
+  res.json({ message: message });
+});
+
+// Time server
 app.get(
   '/now',
   function(req, res, next) {
@@ -11,5 +39,12 @@ app.get(
     res.json({ time: req.time });
   }
 );
+
+// Echo server
+app.get('/:word/echo', function(req, res) {
+  res.json({
+    echo: req.params.word
+  });
+});
 
 module.exports = app;
