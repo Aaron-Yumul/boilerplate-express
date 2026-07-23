a2bfcf8308516295bb3a01765837ee58ecf5ed8e
Aaron John Yumul <aaronjohnyumul526@gmail.com>
2026-07-23 10:33:35 +0800

    Add JSON route for '/json' endpoint
    Added a JSON route to respond with a message.
    
    Signed-off-by: Aaron John Yumul <aaronjohnyumul526@gmail.com>

--- myApp.js
+++ myApp.js
@@ -4,8 +4,16 @@ let app = express();
 // Serve static files
 app.use('/public', express.static(__dirname + '/public'));
 
+// Home page
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
+});
+
+// JSON route
+app.get('/json', function(req, res) {
+  res.json({
+    message: 'Hello json'
+  });
 });
 
 module.exports = app;
