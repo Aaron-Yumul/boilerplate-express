4175bb4f3ed6e0e882874170d547d5fac3a1e402
Aaron John Yumul <aaronjohnyumul526@gmail.com>
2026-07-23 10:36:33 +0800

    Add logger middleware for request logging
    Added root-level logger middleware to log request details.
    
    Signed-off-by: Aaron John Yumul <aaronjohnyumul526@gmail.com>

--- myApp.js
+++ myApp.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
-require('dotenv').config();
+// Root-level logger middleware
+app.use(function(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
 
 // Serve static files
 app.use('/public', express.static(__dirname + '/public'));
@@ -15,16 +19,13 @@ app.get('/', function(req, res) {
 
 // JSON route
 app.get('/json', function(req, res) {
-
   let message = "Hello json";
 
   if (process.env.MESSAGE_STYLE === "uppercase") {
     message = message.toUpperCase();
   }
 
-  res.json({
-    message: message
-  });
+  res.json({ message: message });
 });
 
 module.exports = app;
